058d441d6352d01490ce83148ea8492cbae6bb37
jmccune <justin.mccune@gmail.com>
2015-09-24 15:54:01 -0400

    Display data obtained from back-end.

--- app/routes/content/view.js
+++ app/routes/content/view.js
@@ -1,15 +1,25 @@
 import Ember from 'ember';
-
 import {Constants} from 'dxref/dxref-config';
 import theDataService from 'dxref/services/data-service';
-import listItemModel from 'dxref/models/list-item-model';
-import PagedItems from 'dxref/models/paged-items';
-import MS from 'dxref/services/model-service';
-
+import decorationAdapter from 'dxref/adapters/decoration-adapter';
+import theDecorationService from 'dxref/services/decoration-service';
 
 export default Ember.Route.extend({
-  model: function(){
-   
-  	return theDataService.simulateDelayedResponse(1000,{});
+  model: function(params){
+
+  	var _this= this;
+  	return theDataService.getData(Constants.DXREF_SERVICE,'/contents/'+params.content_id)
+  	.then(function(data) {
+
+    	var decorationSpec = decorationAdapter.convertNreContentToDecoratedContentSpec(data);
+        var decoratedText = theDecorationService.getDecoratedText(decorationSpec);
+        _this.set('decoratedText',decoratedText);  
+        return {
+        	decoratedText: decoratedText
+        };
+    });
+
+  	//return theDataService.simulateDelayedResponse(1000,{});
+
   }
 });
