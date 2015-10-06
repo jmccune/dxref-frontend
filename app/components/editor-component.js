73eaa4a6cde04ef03bc5b51a24aaccfef7d30a1e
jmccune <justin.mccune@gmail.com>
2015-10-06 17:29:17 -0400

    Change using the wysihtml editor to be a component.

---
+++ app/components/editor-component.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({	
+	isExpanded:false,
+	actions: {
+		toggle: function() {			
+			var isExpanded = this.get('isExpanded');
+			isExpanded = !isExpanded;
+			this.set('isExpanded',isExpanded);
+		}
+	},
+	
+	didInsertElement:function(el) {		
+		console.dir(this);
+		var editor = $(this.element).find('.my-editor');
+		var toolbar = $(this.element).find('.my-toolbar');
+
+		var editor = new wysihtml5.Editor(editor.get(0), {
+    			toolbar: toolbar.get(0),
+    			parserRules:  wysihtml5ParserRules
+  		}); 			
+	}
+});
