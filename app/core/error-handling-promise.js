d7fbf0c4c69d479f21c649c0327b10f91e346ab4
jmccune <justin.mccune@gmail.com>
2016-01-02 11:08:59 -0500

    complete error-handling promise tests   + works with simple cases (linear then/catch/finally).   - isn't guaranteed to deal with more complex cases (forked promises, etc.)

--- app/core/error-handling-promise.js
+++ app/core/error-handling-promise.js
@@ -5,37 +5,67 @@ export function ErrorHandlingPromise(actualPromise, rootErrorHandlingPromise, pr
 	} else {
 		this.rootErrorHandlingPromise = this;
 	}
-	this.catchFn = null;
-	this.caughtPromise = null;
+	this.defaultCatchInfo = null;
+	this.catchInfo = null;	
 	this.previousErrorHandlingPromise = previousErrorHandlingPromise;
+
+	// Assume until the next (then/finally) call that we are a terminal
+	this.isTerminal = true;
 }
 
 
 ErrorHandlingPromise.prototype.then=function(successFn,rejectFn,label) {
 	var _this = this;
 	var newPromise = this.actualPromise.then(successFn,rejectFn,label).catch(function(val) {
-		return _this.catchProxyCall(val);
+		return _this._catchProxyCall(val);
 	},label);
-	return new ErrorHandlingPromise(newPromise,this.rootErrorHandlingPromise, this);
 
+	if (rejectFn && this.previousErrorHandlingPromise) {
+		this.previousErrorHandlingPromise.isTerminal =false;
+	}
+
+	return new ErrorHandlingPromise(newPromise,this.rootErrorHandlingPromise, this);
 };
 
 ErrorHandlingPromise.prototype.catch=function(rejectFn,label) {
+	this.previousErrorHandlingPromise._setUserDefinedCatchCall(rejectFn,label);
+	return this;
+};
+
+ErrorHandlingPromise.prototype.finally=function(finallyFn,label) {
 	var _this = this;
-	var newPromise = this.actualPromise.catch(function(val) {
-		return _this.catchProxyCall(val);
+	var newPromise = this.actualPromise.finally(finallyFn,label).catch(function(val) {
+		return _this._catchProxyCall(val);
 	},label);
-	return new ErrorHandlingPromise(newPromise,this.rootErrorHandlingPromise);
+
+	return new ErrorHandlingPromise(newPromise,this.rootErrorHandlingPromise, this);
 };
 
-ErrorHandlingPromise.prototype.finally=function(rejectFn,label) {
-	console.log("FINALLY: "+rejectFn+" "+label);
+ErrorHandlingPromise.prototype.setDefaultCatch=function(rejectFn,label) {
+	this.rootErrorHandlingPromise.defaultCatchInfo = {
+		rejectFn: rejectFn,
+		label: label
+	};
 };
 
-ErrorHandlingPromise.prototype.defaultCatch=function(rejectFn,label) {
-	console.log("FINALLY: "+rejectFn+" "+label);
+ErrorHandlingPromise.prototype._setUserDefinedCatchCall=function(rejectFn,label) {
+	this.catchInfo = {
+		rejectFn: rejectFn,
+		label: label
+	};
 };
 
-ErrorHandlingPromise.prototype.catchProxyCall=function(value) {
+ErrorHandlingPromise.prototype._catchProxyCall=function(value) {
+	//console.log("**** CATCHING PROXY CALL: "+value+ " has catchInfo? "+this.catchInfo);
+	if (this.catchInfo) {
+	//	console.log("LOCAL CATCH");
+		return this.catchInfo.rejectFn(value,this.catchInfo.label);
+	}
+	if (this.isTerminal && this.rootErrorHandlingPromise.defaultCatchInfo) {
+	//	console.log("DEFAULT CATCH");
+		var catchInfo = this.rootErrorHandlingPromise.defaultCatchInfo;
+		return catchInfo.rejectFn(value,catchInfo.label);
+	}
+	//console.log("NO CATCH");
 	throw value;
 };
--- tests/unit/core/error-handling-promise-test.js
+++ tests/unit/core/error-handling-promise-test.js
@@ -4,6 +4,12 @@ import { ErrorHandlingPromise } from 'dxref/core/error-handling-promise';
 
 module('Unit | Core | ErrorHandlingPromise');
 
+
+/** Changing this flag changes whether we return the NORMAL Ember Promise
+    vs. returning the wrapped ErrorHandlingPromise.   Tests that use the
+    defaultCatchHandler are ignored if not using the error handling promise. */
+var USE_ERROR_HANDLING_PROMISE = true;
+
 var generateAsyncProcessMock=function() {
 	var simulatedAsyncProcess = {
 		resolveFn: null,
@@ -28,21 +34,26 @@ var generatePromise=function(simulatedAsyncProcess) {
 		simulatedAsyncProcess.rejectFn = reject;
 	});	
 
-	// if (true) {
-	// 	return promise;
-	// }
+	if (!USE_ERROR_HANDLING_PROMISE) {
+		return promise;
+	}
 
 	var ePromise = new ErrorHandlingPromise(promise);
 	return ePromise;
 };
 
-var generateResponseFn=function(logObject,tagForFnCalled,throwMessage) {
+var generateResponseFn=function(logObject,tagForFnCalled,throwMessage, xformedResponse) {
 	return function(x) {	
 		logObject.logCalls+='>'+tagForFnCalled+'('+JSON.stringify(x)+')';
 		if (throwMessage) {
 			throw throwMessage;
 		}
-		return x;
+		if (xformedResponse) {
+			return xformedResponse;
+		}
+		if (x) {
+			return x;
+		}
 	};
 };
 
@@ -212,3 +223,156 @@ test('validate basic promises S1C1', function(assert) {
 					.catch(generateResponseFn(lo,'catch1'));
 						  
 });
+
+
+/**  Successful Ajax Response, the (SUCCESS) handler works properly*/
+test('validate basic promises S1NC1', function(assert) {
+
+	var testData = testHarnessFn(assert,'S1NC1',{success: true, data: 'S1NC1'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'S1NC1>>success0(\"S1NC1\")');
+	});
+
+	var lo = testData.logObject;
+
+	testData.promise.then(generateResponseFn(lo,'success0'),
+						  generateResponseFn(lo,'reject0'))
+					.catch(generateResponseFn(lo,'catch1'));
+						  
+});
+
+/** Finally works (success, no error on handling) */
+test('validate basic promises finally S1F2', function(assert) {
+
+	var testData = testHarnessFn(assert,'S1F2',{success: true, data: 'S1F2'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'S1F2>>success0(\"S1F2\")>finally1(undefined)');
+	});
+
+	var lo = testData.logObject;
+
+	testData.promise.then(generateResponseFn(lo,'success0'),
+						  generateResponseFn(lo,'reject0'))
+					.finally(generateResponseFn(lo,'finally1'));
+						  
+});
+
+
+/** Finally works (failure case, no error on handling) */
+test('validate basic promises finally R1TF2', function(assert) {
+
+	var testData = testHarnessFn(assert,'R1TF2',{success: false, data: 'R1TF2'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'R1TF2>>reject0(\"R1TF2\")>finally1(undefined)');
+	});
+
+	var lo = testData.logObject;
+
+	testData.promise.then(generateResponseFn(lo,'success0'),
+						  generateResponseFn(lo,'reject0'))
+					.finally(generateResponseFn(lo,'finally1'));
+						  
+});
+
+// ==============================================
+// DEFAULT ERROR HANDLING SPECIFIC TEST-CASES
+// ==============================================
+
+
+/**  Successful Ajax Response, the (SUCCESS) handler throws an error, the DEFAULT error handler picks up 
+	because the user defined no CATCH handler themselves.... */
+test('validate ERROR HANDLING promise w Default Catch EHDC1', function(assert) {
+
+	if (!USE_ERROR_HANDLING_PROMISE) {
+		assert.expect(0);
+		return;
+	}
+
+	var testData = testHarnessFn(assert,'EHDC1',{success: true, data: 'EHDC1'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'EHDC1>>success0(\"EHDC1\")>DefaultCatchEHDC1(\"SH_errorEHDC1\")');
+	});
+
+	var lo = testData.logObject;
+
+	testData.promise.setDefaultCatch(generateResponseFn(lo,'DefaultCatchEHDC1'));	
+	testData.promise.then(generateResponseFn(lo,'success0','SH_errorEHDC1'));		  
+});
+
+/**  Successful Ajax Response, the (SUCCESS) handler throws an error, the DEFAULT error handler picks up 
+	because the user defined no CATCH handler themselves.... */
+test('validate ERROR HANDLING promise w Default Catch EHDC2', function(assert) {
+
+	if (!USE_ERROR_HANDLING_PROMISE) {
+		assert.expect(0);
+		return;
+	}
+
+	var testData = testHarnessFn(assert,'EHDC2',{success: true, data: 'EHDC2'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'EHDC2>>success0(\"EHDC2\")>success1(\"EHDC2\")>success2(\"EHDC2\")>HANDLED-ERROR(\"errorSH_2\")>success3(\"EHDC2-error-handled\")>success4(\"EHDC2-error-handled\")');
+	});
+
+	var lo = testData.logObject;
+
+	testData.promise.setDefaultCatch(generateResponseFn(lo,'HANDLED-ERROR',null,'EHDC2-error-handled'));	
+	testData.promise.then(generateResponseFn(lo,'success0'))  // Success0(EHDC2)
+		.then(generateResponseFn(lo,'success1'))			  // + Success1(EHCD2)
+		.then(generateResponseFn(lo,'success2','errorSH_2'))  // + Success2(EHDC2) + HANDLED-ERROR(errorSH_2)
+		.then(generateResponseFn(lo,'success3'))			 //  + Success3(EHDC2-error-handled)
+		.then(generateResponseFn(lo,'success4'));			 //  + Success4(EHDC2-error-handled)
+
+
+});
+
+
+
+/**  COMPLEX CHAINING  */
+test('validate basic promise w similar test case to EHDC3', function(assert) {
+
+	var testData = testHarnessFn(assert,'TC3',{success: true, data: 'TC3'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'TC3>>success0(\"TC3\")>success1(\"TC3\")>error_handle2(\"errorSH_1\")>success3(\"direct-handled-error\")>catch_handler3(\"errorSH_3\")>success4(\"catch-handled-error\")>terminal(\"terminal-catch-should-catch-me\")');
+	});
+
+	var lo = testData.logObject;
+	
+	testData.promise.then(generateResponseFn(lo,'success0'))  
+		.then(generateResponseFn(lo,'success1','errorSH_1'))  
+		.then(generateResponseFn(lo,'success2'), 
+			  generateResponseFn(lo,'error_handle2',null,'direct-handled-error'))		
+		.then(generateResponseFn(lo,'success3','errorSH_3'))
+		.catch(generateResponseFn(lo,'catch_handler3',null,'catch-handled-error'))			
+		.then(generateResponseFn(lo,'success4','terminal-catch-should-catch-me'))
+		.catch(generateResponseFn(lo,'terminal'));
+
+
+});
+
+/**  Successful Ajax Response, the (SUCCESS) handler throws an error, the DEFAULT error handler picks up 
+	because the user defined no CATCH handler themselves.... */
+test('validate ERROR HANDLING promise w Default Catch EHDC3', function(assert) {
+
+	if (!USE_ERROR_HANDLING_PROMISE) {
+		assert.expect(0);
+		return;
+	}
+
+	var testData = testHarnessFn(assert,'EHDC3',{success: true, data: 'EHDC3'},function(data) {
+		//EXPECT all the calls to do this...
+		assert.strictEqual(data,'EHDC3>>success0(\"EHDC3\")>success1(\"EHDC3\")>error_handle2(\"errorSH_1\")>success3(\"direct-handled-error\")>catch_handler3(\"errorSH_3\")>success4(\"catch-handled-error\")>DEFAULT-HANDLED-ERROR(\"final-error\")');
+	});
+
+	var lo = testData.logObject;
+
+	testData.promise.setDefaultCatch(generateResponseFn(lo,'DEFAULT-HANDLED-ERROR',null,'EHDC3-error-handled'));	
+	testData.promise.then(generateResponseFn(lo,'success0'))  // Success0(EHDC2)
+		.then(generateResponseFn(lo,'success1','errorSH_1'))			  // + Success1(EHCD2)
+		.then(generateResponseFn(lo,'success2'), 
+			  generateResponseFn(lo,'error_handle2',null,'direct-handled-error'))		
+		.then(generateResponseFn(lo,'success3','errorSH_3'))
+		.catch(generateResponseFn(lo,'catch_handler3',null,'catch-handled-error'))			
+		.then(generateResponseFn(lo,'success4','final-error'));			
+
+});
+
